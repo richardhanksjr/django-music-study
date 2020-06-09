0f9b9b8133c6b332c948358ca84e8d365ac8ddaa
Richard Hanks <richardhanksjr@gmail.com>
2020-06-09 15:13:47 -0400

    Trying to add /code/ to the beginning of the static_url.

--- musicstudy/settings.py
+++ musicstudy/settings.py
@@ -130,7 +130,7 @@ STATICFILES_FINDERS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 print("static_root", STATIC_ROOT)
-STATIC_URL = '/static/'
+STATIC_URL = '/code/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
