dc727473932d1703f3a7f968da73b8eea93580f9
Richard Hanks <richardhanksjr@gmail.com>
2020-06-09 14:52:18 -0400

    Adding print debug statement for static_url

--- musicstudy/settings.py
+++ musicstudy/settings.py
@@ -129,6 +129,7 @@ STATICFILES_FINDERS = [
 # # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print("static_root", STATIC_ROOT)
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
