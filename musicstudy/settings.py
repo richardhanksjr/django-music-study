bce383f41602a85efa88459edb5235b1f1ad48ba
Richard Hanks <richardhanksjr@gmail.com>
2020-06-09 11:02:21 -0400

    Added STATICFILES_DIRS to settings file.

--- musicstudy/settings.py
+++ musicstudy/settings.py
@@ -126,6 +126,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if ENVIRONMENT == 'production':
